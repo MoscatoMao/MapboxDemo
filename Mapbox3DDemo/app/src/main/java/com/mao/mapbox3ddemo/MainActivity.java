5cee908b7ea9e7044039d9888db6db98201d862c
MoscatoMao <37256525+MoscatoMao@users.noreply.github.com>
2018-03-11 12:02:25 +0800

    Add files via upload

---
+++ Mapbox3DDemo/app/src/main/java/com/mao/mapbox3ddemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.mao.mapbox3ddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.access_token));
+        setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+    }
+}
